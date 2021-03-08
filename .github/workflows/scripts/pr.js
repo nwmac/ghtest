2be0c2517cbc5fda509cbfd634a8f613625c73d0
Neil MacDougall <macdougall.neil@gmail.com>
2021-03-08 15:28:02 +0000

    debug

--- .github/workflows/scripts/pr.js
+++ .github/workflows/scripts/pr.js
@@ -2,6 +2,14 @@ console.log('NODE Action running...');
 
 const event = require(process.env.GITHUB_EVENT_PATH);
 
-console.log(event);
+console.log(JSON.stringify(event, null, 2));
 
+
+const body = event.pull_request.body;
+
+console.log('body is');
+console.log(body);
+
+
+console.log('-----');
 console.log('Process finished');
